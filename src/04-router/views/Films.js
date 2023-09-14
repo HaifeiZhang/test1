7e02903f65d353a0684c524df87ad7ca92ee39bd
Haifei Zhang <haifei.zhang1028@gmail.com>
2023-09-14 23:14:29 +0200

    P71

--- src/04-router/views/Films.js
+++ src/04-router/views/Films.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Route, Switch,Redirect } from 'react-router-dom/cjs/react-router-dom.min'
+import NowPlaying from './Films/NowPlaying'
+import ComingSoon from './Films/ComingSoon'
 
 export default function Films() {
   return (
-    <div>Films</div>
+    <div>
+      <div style={{background:"yellow",height:"100px"}}>Films</div>
+      <Switch>
+        <Route path="/films/nowplaying" component={NowPlaying}></Route>
+        <Route path="/films/comingsoon" component={ComingSoon}></Route>
+        <Redirect from="/films" to="/films/nowplaying" exact></Redirect>
+      </Switch>
+    </div>
   )
 }
